b9bda547d5daa3d2e5afe377916e653015a1beca
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2023-03-06 17:17:03 +0530

    modified:   solution/gencsv.sh

--- solution/gencsv.sh
+++ solution/gencsv.sh
@@ -3,6 +3,18 @@
 START=${1}
 END=${2}
 
+if ! [[ ${START} =~ ^[+-]?[0-9]+$ ]]; 
+   then echo "error: ${START} is not a number" >&2; exit 1 
+fi
+
+if ! [[ ${END} =~ ^[+-]?[0-9]+$ ]]; 
+   then echo "error: ${END} is not a number" >&2; exit 1 
+fi
+
+if [ -f "inputFile" ]; then
+  rm inputFile
+fi
+
 while [ "${START}" -le "${END}" ]; do
   echo "${START}, ${RANDOM}" >> inputFile
   ((START++))
